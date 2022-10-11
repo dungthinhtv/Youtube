b4dea463d7caf21fcfd927c7eb6ee2a952b8a0a0
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-11 22:42:44 +0700

    1 file modified

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import videoRoutes from './routes/videos.js';
 import commentRoutes from './routes/comments.js';
 import authRoutes from './routes/auth.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+
+// import cors from 'cors';
 
 const app = express();
+const cors = require('cors');
+
 dotenv.config();
 
 const connect = () => {
@@ -29,6 +32,7 @@ const connect = () => {
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
